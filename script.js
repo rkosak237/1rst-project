ba64a56c2cbaae7ad7f5da8c38676c5ceb9e8ad0
rkosak237 <robert.kosak237@gmail.com>
2017-02-20 17:11:53 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
-$(document).ready(function() {
-  $('#pubs').click(function() {
-    $(this).addClass('red');
-  });
-});
+var marginY = 0;
+var destination = 0;
+var speed = 10;
+var scroller = null;
+
+function initScroll(elementId) {
+  destination = document.getElementById(elementId).offsetTop;
+
+  scroller = setTimeout(function() {
+    initScroll(elementId);
+  }, 1);
+
+  marginY = marginY + speed;
+
+  if(marginY >= destination) {
+    clearTimeout(scroller);
+  }
+  window.scroll(0, marginY);
+}
+
+//to top
+function goTop() {
+  scroller = setTimeout(function(){
+    goTop();
+  }, 1);
+
+  marginY = marginY - speed;
+
+  if(marginY <= 0) {
+    clearTimeout(scroller);
+  }
+  window.scroll(0, marginY);
+}
