cd4e916bc2cf7eca88575e6e1a52214504e831e7
rkosak237 <robert.kosak237@gmail.com>
2017-08-09 17:11:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $("nav").wrap('<div class="nav-ph"></div>');
   $(".nav-ph").height($("nav").outerHeight());
 
-// hide nav when user is scrolling down, show when he's scrolling up
+
 var mywindow = $(window);
 var mypos = mywindow.scrollTop();
 var up = false;
@@ -40,7 +40,6 @@ $('.nav-hidden').click(function() {
   /*gallery filter start*/
   $(".first-gal-nav").click(function(){
     var category = $(this).attr("id")
-
     if (category == "all") {
       $(".category_item").addClass("hide");
       setTimeout(function() {
@@ -54,39 +53,9 @@ $('.nav-hidden').click(function() {
     }
 
   });
-// Shuffle start
-var testAr = ['gallery/bar.jpg', 'gallery/basketball.jpg', 'gallery/forest.jpg', 'gallery/library.jpg', 'gallery/mural.jpg', 'gallery/woods.jpg', 'gallery/street.jpg', 'gallery/forest1.jpg', 'gallery/forest2.jpeg', 'gallery/bw_1.jpg', 'gallery/forest3.jpg', 'gallery/bw_2.jpeg', 'gallery/street1.jpeg', 'gallery/forest4.jpeg', 'gallery/street2.jpeg', 'gallery/various1.jpeg'];
-
-
-function shuffle() {
-  let randomize = testAr.sort(function() {
-    return 0.5 - Math.random()
-  });
-  randomize.toString();
-  document.getElementById("gallery").innerHTML = testAr;
-  const spaceAr = testAr.join(", ");
-
-}
-
-function printOut() {
-  var imgs = "";
-
-      for (var i = 0; i < testAr.length; i++) {
-          var img = document.createElement('img');
-          img.setAttribute('src', testAr[i]);
-          img.setAttribute('class', 'category_item');
-          imgs += img.outerHTML;
-      }
-
-      var container = document.getElementById('gallery');
-      container.innerHTML = imgs;
-}
-
-const btnCheck = document.getElementById('mix').addEventListener('click', shuffle);
-const print = document.getElementById('mix').addEventListener('click', printOut);
 
 // Shuffle stop
-$('.button-middle').click(function(){
+$('.card-btn-cta').click(function(){
     alert('yup, its working');
   })
     /*gallery filter stop*/
